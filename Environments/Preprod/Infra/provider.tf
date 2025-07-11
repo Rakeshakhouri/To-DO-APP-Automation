2fe7299a393574438a68c6bf93f60f7e4248da8b
RakeshAKhouri <rakhouri472@gmail.com>
2025-07-11 10:32:03 +0530

    added Todo app

---
+++ Environments/Preprod/Infra/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.32.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "4972e01e-b3c0-45e3-becf-c21112caced1"
+}
